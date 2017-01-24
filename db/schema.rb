c85071ddafa9b869084843649ed2166393afb753
Carson Hamilton <cjcham77@gmail.com>
2017-01-24 14:56:25 -0700

    test for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170104001341) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chef_reviews", force: :cascade do |t|
     t.integer  "chef_id"
     t.integer  "user_id"
@@ -42,8 +45,8 @@ ActiveRecord::Schema.define(version: 20170104001341) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.index ["email"], name: "index_chefs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_chefs_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_chefs_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_chefs_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "requests", force: :cascade do |t|
@@ -85,8 +88,8 @@ ActiveRecord::Schema.define(version: 20170104001341) do
     t.string   "phonenumber"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
